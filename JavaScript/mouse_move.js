ccbe03cf5e49708633639c431f9d7b9e91623d62
janata.simon04@gmail.com <janata.simon04@gmail.com>
2021-11-07 12:51:19 +0100

    upravil jsem pohyb mysi

--- JavaScript/mouse_move.js
+++ JavaScript/mouse_move.js
@@ -2,7 +2,7 @@ document.addEventListener("mousemove", function(e) {
     const content = document.querySelector('.content');
     const cpu = document.querySelector('.cpu');
 
-    cpu.style.right = 100 + e.pageX/2 + 'px';
+    cpu.style.right = 80 + e.pageX/2 + 'px';
 
-    content.style.left = 100 + e.pageX/2 + 'px';
+    content.style.left = 80 + e.pageX/2.25 + 'px';
 })
