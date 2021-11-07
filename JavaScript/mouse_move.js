5689148957785e900fa40092797871df48ffb503
janata.simon04@gmail.com <janata.simon04@gmail.com>
2021-11-07 17:44:31 +0100

    upravena dojezdnost k leve strane

--- JavaScript/mouse_move.js
+++ JavaScript/mouse_move.js
@@ -2,7 +2,7 @@ document.addEventListener("mousemove", function(e) {
     const content = document.querySelector('.content');
     const cpu = document.querySelector('.cpu');
 
-    cpu.style.right = 80 + e.pageX/2 + 'px';
+    cpu.style.right = 80 + e.pageX/1.75 + 'px';
 
     content.style.left = 80 + e.pageX/2 + 'px';
 })
